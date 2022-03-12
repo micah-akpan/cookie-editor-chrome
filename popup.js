1a98136d47d7a8a2351f02d2cfe4e64df5211810
Micah <micahngabriel@gmail.com>
2022-03-11 20:26:04 -0600

    Prettify

--- popup.js
+++ popup.js
@@ -1,157 +1,180 @@
-const cookieKey = document.getElementById('tb-cookie-key')
-const getCookieBtn = document.getElementById('btn-get-cookie')
+const cookieKey = document.getElementById("tb-cookie-key");
+const getCookieBtn = document.getElementById("btn-get-cookie");
 
-const cookieIndex = document.getElementById('tb-cookie-index')
-const newCookieValue = document.getElementById('tb-cookie-value')
+const cookieIndex = document.getElementById("tb-cookie-index");
+const newCookieValue = document.getElementById("tb-cookie-value");
 
-const cookieCopyMsg = document.getElementById('cookie-copy-msg')
-const cookieHostURLElem = document.getElementById('tb-cookie-host-url')
+const cookieCopyMsg = document.getElementById("cookie-copy-msg");
+const cookieHostURLElem = document.getElementById("tb-cookie-host-url");
 
-const cookieURLSaveCheckbox = document.getElementById('cb-cookie-save')
-const cookieTextError = document.getElementById('text-cookie-error')
+const cookieURLSaveCheckbox = document.getElementById("cb-cookie-save");
+const cookieTextError = document.getElementById("text-cookie-error");
 
-const overwriteCookieCheckBox = document.getElementById('cb-cookie-overwrite-old')
+const overwriteCookieCheckBox = document.getElementById(
+  "cb-cookie-overwrite-old"
+);
 
-const COOKIE_HOST_URL = 'cookieHostURL'
+const COOKIE_HOST_URL = "cookieHostURL";
 
-document.forms.item(0).addEventListener('submit', async (e) => {
-  e.preventDefault()
-  cookieCopyMsg.textContent = ''
+document.forms.item(0).addEventListener("submit", async (e) => {
+  e.preventDefault();
+  cookieCopyMsg.textContent = "";
   try {
-    const canSaveHostURL = cookieURLSaveCheckbox.checked
-    await saveOrClearCookieHostURL(canSaveHostURL)
+    const canSaveHostURL = cookieURLSaveCheckbox.checked;
+    await saveOrClearCookieHostURL(canSaveHostURL);
 
-    const updatedCookie = await getNewCookieValue()
-    setUpdatedCookieValue(updatedCookie)
+    const updatedCookie = await getNewCookieValue();
+    setUpdatedCookieValue(updatedCookie);
 
-    const cookieName = getCookieName()
-    const cookieHostURL = getCookieHostURL()
+    const cookieName = getCookieName();
+    const cookieHostURL = getCookieHostURL();
 
-    const cookie = await getCookie(cookieHostURL, cookieName)
-    const parsedCurrentCookie = JSON.parse(JSON.stringify(cookie))
+    const cookie = await getCookie(cookieHostURL, cookieName);
+    const parsedCurrentCookie = JSON.parse(JSON.stringify(cookie));
 
-    const allowListOfCookiePropsObj = getAllowListOfCookieProps(parsedCurrentCookie)
+    const allowListOfCookiePropsObj =
+      getAllowListOfCookieProps(parsedCurrentCookie);
 
     // overwrite cookie if enabled
     if (overwriteCookieCheckBox.checked) {
       await chrome.cookies.set({
-          ...allowListOfCookiePropsObj,
-          url: cookieHostURL,
-          name: cookieName,
-          value: updatedCookie,
-      })
+        ...allowListOfCookiePropsObj,
+        url: cookieHostURL,
+        name: cookieName,
+        value: updatedCookie,
+      });
     }
   } catch (error) {
-    cookieTextError.innerHTML = error.message
+    cookieTextError.innerHTML = error.message;
   }
-})
+});
 
-window.addEventListener('load', async () => {
+window.addEventListener("load", async () => {
   try {
-    const cookieHostURL = await chrome.storage.local.get([COOKIE_HOST_URL])
+    const cookieHostURL = await chrome.storage.local.get([COOKIE_HOST_URL]);
     if (cookieHostURL.cookieHostURL) {
-      cookieURLSaveCheckbox.setAttribute('checked', true)
-      cookieHostURLElem.value = cookieHostURL.cookieHostURL
+      cookieURLSaveCheckbox.setAttribute("checked", true);
+      cookieHostURLElem.value = cookieHostURL.cookieHostURL;
     }
   } catch (error) {
-    cookieTextError.innerHTML = error.message
+    cookieTextError.innerHTML = error.message;
   }
-})
+});
 
-chrome.cookies.onChanged.addListener(
-  () => {
-    cookieCopyMsg.classList.add('show-block')
-    cookieCopyMsg.innerHTML = 'You now have the new cookie in store!'
-  }
-)
+chrome.cookies.onChanged.addListener(() => {
+  cookieCopyMsg.classList.add("show-block");
+  cookieCopyMsg.innerHTML = "You now have the new cookie in store!";
+});
 
 function getNewCookieElem() {
   if (!navigator.clipboard) {
-    const getCookieBtnText = document.getElementById('text-new-cookie')
-    getCookieBtnText.classList.add('show-block')
-    return getCookieBtnText
+    const getCookieBtnText = document.getElementById("text-new-cookie");
+    getCookieBtnText.classList.add("show-block");
+    return getCookieBtnText;
   }
 
-  const newCookieBtn = document.getElementById('btn-new-cookie')
-  newCookieBtn.classList.add('show-block')
+  const newCookieBtn = document.getElementById("btn-new-cookie");
+  newCookieBtn.classList.add("show-block");
 
-  newCookieBtn.addEventListener('click', (e) => {
-    copyCookieToClipboard(e.target.textContent.trim())
-  })
+  newCookieBtn.addEventListener("click", (e) => {
+    copyCookieToClipboard(e.target.textContent.trim());
+  });
 
-  return newCookieBtn
+  return newCookieBtn;
 }
 
 function getAllowListOfCookieProps(cookieObj) {
-  const allowPropsList = ['domain', 'expirationDate', 'httpOnly', 'name', 'path', 'sameSite', 'secure', 'storeId', 'url', 'value']
+  const allowPropsList = [
+    "domain",
+    "expirationDate",
+    "httpOnly",
+    "name",
+    "path",
+    "sameSite",
+    "secure",
+    "storeId",
+    "url",
+    "value",
+  ];
 
-  const allowPropsObj = {}
+  const allowPropsObj = {};
   allowPropsList.forEach((item) => {
-    allowPropsObj[item] = cookieObj[item]
-  })
+    allowPropsObj[item] = cookieObj[item];
+  });
 
-  return allowPropsObj
+  return allowPropsObj;
 }
- 
 
 async function getCookie(cookieHostURL, cookieName) {
   const cookie = await chrome.cookies.get({
     url: cookieHostURL,
-    name: cookieName
-  })
+    name: cookieName,
+  });
 
   if (cookie === null) {
-    throw new Error(`There's probably no cookie with the name in the specified domain: ${cookieName}`)
+    throw new Error(
+      `There's probably no cookie with the name in the specified domain: ${cookieName}`
+    );
   }
 
-  return cookie
+  return cookie;
 }
 
 function getCookieName() {
-  return cookieKey.value
+  return cookieKey.value;
 }
 
 function getCookieHostURL() {
-  return cookieHostURLElem.value
+  return cookieHostURLElem.value;
 }
-
 
 function setUpdatedCookieValue(newCookie) {
-  const newCookieValueElem = newCookieValue.insertAdjacentElement('afterend', getNewCookieElem())
-  newCookieValueElem.textContent = newCookie
+  const newCookieValueElem = newCookieValue.insertAdjacentElement(
+    "afterend",
+    getNewCookieElem()
+  );
+  newCookieValueElem.textContent = newCookie;
 }
 
-
 async function getNewCookieValue() {
-  const cookieHostURLValue = await chrome.storage.local.get(['cookieHostURL'])
+  const cookieHostURLValue = await chrome.storage.local.get(["cookieHostURL"]);
 
-  let cookieToBeChanged = ''
+  let cookieToBeChanged = "";
 
   if (cookieHostURLValue.cookieHostURL) {
-    const sanitizedCookieHostURLValue = cookieHostURLValue.cookieHostURL.trim()
-    const cookie = await getCookie(sanitizedCookieHostURLValue, cookieKey.value)
-    cookieToBeChanged = cookie.value
+    const sanitizedCookieHostURLValue = cookieHostURLValue.cookieHostURL.trim();
+    const cookie = await getCookie(
+      sanitizedCookieHostURLValue,
+      cookieKey.value
+    );
+    cookieToBeChanged = cookie.value;
   } else {
     // no stored cookie, use the one from the input field
-    const cookie = await getCookie(cookieHostURLElem.value.trim(), cookieKey.value)
-    cookieToBeChanged = cookie.value
+    const cookie = await getCookie(
+      cookieHostURLElem.value.trim(),
+      cookieKey.value
+    );
+    cookieToBeChanged = cookie.value;
   }
 
-  const mutatedCookie = mutate(cookieToBeChanged, cookieIndex.value, newCookieValue.value)
+  const mutatedCookie = mutate(
+    cookieToBeChanged,
+    cookieIndex.value,
+    newCookieValue.value
+  );
 
-  return mutatedCookie
-
+  return mutatedCookie;
 }
 
 /**
  * @description it changes the `index`th value of `cookie` to `newValue`
- * @param {string} cookie 
- * @param {number} index 
- * @param {any} newValue 
+ * @param {string} cookie
+ * @param {number} index
+ * @param {any} newValue
  * @returns {string}
  */
 function mutate(cookie, index, newValue) {
-  return cookie.substring(0, index) + newValue + cookie.substring(+index + 1)
+  return cookie.substring(0, index) + newValue + cookie.substring(+index + 1);
 }
 
 /**
@@ -162,37 +185,37 @@ function mutate(cookie, index, newValue) {
 async function copyCookieToClipboard(cookie) {
   if (cookie) {
     try {
-      await navigator.clipboard.writeText(cookie)
-      cookieCopyMsg.classList.replace('hide-block', 'show-block')
-      cookieCopyMsg.innerHTML = '<strong>Copied!</strong>'
-      return true
+      await navigator.clipboard.writeText(cookie);
+      cookieCopyMsg.classList.replace("hide-block", "show-block");
+      cookieCopyMsg.innerHTML = "<strong>Copied!</strong>";
+      return true;
     } catch (error) {
-      cookieCopyMsg.classList.add('error')
-      cookieCopyMsg.innerHTML = '<strong>Copy Failed 😢.</strong>'
+      cookieCopyMsg.classList.add("error");
+      cookieCopyMsg.innerHTML = "<strong>Copy Failed 😢.</strong>";
     }
   }
-  return false
+  return false;
 }
 
 /**
- * 
+ *
  * @description saves the cookie Host URL for later use
  * @param {boolean} canSaveHostURL
  * @returns {Promise<void>}
  */
 async function saveOrClearCookieHostURL(canSaveHostURL) {
-  const cookieHostURLValue = cookieHostURLElem.value.trim()
+  const cookieHostURLValue = cookieHostURLElem.value.trim();
 
   if (canSaveHostURL) {
     if (cookieHostURLValue) {
-      await chrome.storage.local.set({ cookieHostURL: cookieHostURLValue })
+      await chrome.storage.local.set({ cookieHostURL: cookieHostURLValue });
     } else {
-      throw new Error('Please provide a cookie host URL')
+      throw new Error("Please provide a cookie host URL");
     }
   } else {
-    const cookieHostURL = await getStoredCookieHostURL()
+    const cookieHostURL = await getStoredCookieHostURL();
     if (cookieHostURL) {
-      await chrome.storage.local.remove('cookieHostURL')
+      await chrome.storage.local.remove("cookieHostURL");
     }
   }
 }
@@ -202,6 +225,6 @@ async function saveOrClearCookieHostURL(canSaveHostURL) {
  * @returns {Promise<string>}
  */
 async function getStoredCookieHostURL() {
-  const cookieHostURLValue = await chrome.storage.local.get([COOKIE_HOST_URL])
-  return cookieHostURLValue.cookieHostURL
+  const cookieHostURLValue = await chrome.storage.local.get([COOKIE_HOST_URL]);
+  return cookieHostURLValue.cookieHostURL;
 }
